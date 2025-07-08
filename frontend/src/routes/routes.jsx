dbb7ea88ea0fd7d83916107932085d030b6f5863
yukirochi <justineabanilla81@gmail.com>
2025-07-08 22:49:49 +0800

    front structure

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+
+function Router() {
+    return ( <div className="w-[100vw] h-[100vh] bg-red-950">
+           <Routes>
+               <Route path=""  />
+               
+           </Routes>
+    </div> );
+}
+
+export default Router;
