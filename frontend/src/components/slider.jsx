b3f3594e6e798a0b79542d4fe7f1b7796c88d719
yukirochi <justineabanilla81@gmail.com>
2025-07-30 16:16:26 +0800

    more polished game ui

--- frontend/src/components/slider.jsx
+++ frontend/src/components/slider.jsx
@@ -1,29 +1,18 @@
-function Slider() {
+function Slider({value}) {
   return (
     <div className="w-[100%] h-[5%]  flex items-center justify-center flex-col ">
-        <div className="w-[70%] h-[30%] flex justify-between items-center mb-5 font-bold ">
-        <p className="text-[green]">.</p>
-        <p className="text-[green]">.</p>
-        <p className="text-[green]">.</p>
-        <p className="text-[yellow]">.</p>
-        <p className="text-[yellow]">.</p>
-        <p className="text-[yellow]">.</p>
-        <p className="text-[red]">.</p>
-        <p className="text-[red]">.</p>
-        <p className="text-[red]">.</p>
-        <p className="text-[purple]">.</p>
-      </div>
+      
       <div className="w-[70%] h-[30%] flex justify-between items-center mb-5 font-bold">
-        <p >1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
-        <p>5</p>
-        <p>6</p>
-        <p>7</p>
-        <p>8</p>
-        <p>9</p>
-        <p>10</p>
+        <p className="text-[#6c63ff]">Lupa</p>
+        <p className="text-[green]"></p>
+        <p className="text-[green]"></p>
+        <p className="text-[#b69230]"></p>
+        <p className="text-[#b69230]"></p>
+        <p className="text-[#b69230]"></p>
+        <p className="text-[red]"></p>
+        <p className="text-[red]"></p>
+        <p className="text-[red]"></p>
+        <p className="text-[#ff6583]">Alapaap</p>
       </div>
       <input
         type="range"
@@ -31,9 +20,13 @@ function Slider() {
         min="1"
         max="10"
         step="1"
-        value="3"
+        value ={value}
         disabled
       />
+      {value <= 3 && <p className="text-green-950 font-bold">EASY</p>}
+      {value <= 6 && value > 3 && <p className="text-[#b69230] font-bold">MEDIUM</p>}
+      {value <= 9 && value > 6 && <p className="text-[red] font-bold">HARD</p>}
+      {value > 9 && <p className="text-[purple] font-bold">BOSS</p>}
     </div>
   );
 }
--- frontend/src/pages/game.jsx
+++ frontend/src/pages/game.jsx
@@ -4,22 +4,37 @@ import Slider from "../components/slider";
 
 function Game() {
   let [life, setlife] = useState(3);
+  let [level, setlevel] = useState(1)
   return (
     <div className="w-[100vw] h-[100vh] overflow-hidden">
       <Gameheader life={life} />
       <div className="w-[100%] h-[5%]  flex relative">
-        <Slider />
+        <Slider value={level}/>
       </div>
+    
       <div className=" w-[100%] h-[70%] flex flex-col justify-center items-center">
-        <div className="w-[80%] h-[40%] border-[1px] border-black rounded-sm max-w-[500px]"></div>
+        <div className="w-[80%] h-[40%] border-[1px] rounded-sm max-w-[500px] flex justify-center text-center items-center">
+          <p className="w-[80%]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum tenetur eos suscipit, repudiandae modi fuga perferendis dolore sequi beatae mollitia autem nesciunt ad, ipsam ullam iure aut laborum incidunt unde!</p>
+        </div>
         <div className="w-[80%] h-[5%] flex justify-center items-center text-[20px]">
           00:00
         </div>
         <div className="w-[80%] h-[50%] flex flex-col items-center justify-center gap-[5%]">
-          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white]  px-5 py-2 text-sm shadow-sm hover:shadow-lg  font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] text-white hover:text-[#ff6583] rounded-sm transition ease-in duration-300"></div>
-          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white]  px-5 py-2 text-sm shadow-sm hover:shadow-lg  font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] text-white hover:text-[#ff6583] rounded-sm transition ease-in duration-300"></div>
-          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white]  px-5 py-2 text-sm shadow-sm hover:shadow-lg  font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] text-white hover:text-[#ff6583] rounded-sm transition ease-in duration-300"></div>
-          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white]  px-5 py-2 text-sm shadow-sm hover:shadow-lg  font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] text-white hover:text-[#ff6583] rounded-sm transition ease-in duration-300"></div>
+          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white] text-[#ff6583] px-5 py-2 text-sm shadow-sm hover:shadow-lg   flex justify-center items-center font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] hover:text-white  rounded-sm transition ease-in duration-300">
+           <p className="w-[90%]">A. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+          </div>
+          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white] text-[#ff6583] px-5 py-2 text-sm shadow-sm hover:shadow-lg   flex justify-center items-center font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] hover:text-white  rounded-sm transition ease-in duration-300">
+           <p className="w-[90%]">A. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+          </div>
+          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white] text-[#ff6583] px-5 py-2 text-sm shadow-sm hover:shadow-lg   flex justify-center items-center font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] hover:text-white  rounded-sm transition ease-in duration-300">
+           <p className="w-[90%]">A. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+          </div>
+          <div className="w-[100%] h-[15%]  max-w-[500px] bg-[white] text-[#ff6583] px-5 py-2 text-sm shadow-sm hover:shadow-lg   flex justify-center items-center font-medium tracking-wider border-2 border-[#ff6583] hover:bg-[#ff6583] hover:border-[#ff6583] hover:text-white  rounded-sm transition ease-in duration-300">
+           <p className="w-[90%]">A. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+           
+          </div>
+
+         
         </div>
         <div className="w-[100%] h-[5%] flex justify-center items-center flex-col">
           <button className="bg-[#6c63ff] hover:bg-transparent px-5 py-2 text-sm shadow-sm hover:shadow-lg  font-medium tracking-wider border-2 border-[#6c63ff] hover:border-[#6c63ff] text-white hover:text-[#6c63ff] rounded-full transition ease-in duration-300">
