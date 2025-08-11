f45907a852cbf8ca52203a40ade0add2d67a2301
yukirochi <justineabanilla81@gmail.com>
2025-08-12 03:14:54 +0800

    finalized

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://alapaap-t62s.vercel.app/"
 }))
 
 app.use((req, res, next) => {
