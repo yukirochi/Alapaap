68e987e35c0655b2a977446f41653a01c098495f
otto <justineabanilla81@gmail.com>
2025-09-13 13:31:50 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,7 @@ const cors = require("cors")
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://alapaap.vercel.app"
-}))
+app.use(cors())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
